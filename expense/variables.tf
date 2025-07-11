05cc6f87b68c6d41cecda485787fab20850d90d4
Dasari <josephdasari549@gmail.com>
2025-07-12 02:28:32 +0530

    expense project

---
+++ expense/variables.tf
@@ -0,0 +1,22 @@
+variable "instance_names" {
+    type = list(string)
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "zone_id" {
+    default = "Z043101513OWSW7ELREJ9"
+}
+
+variable "domain_name" {
+    default = "dasari.site"
+}
+
